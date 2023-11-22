6adf9702e011992aaf0984ed87784051c618a7b0
Nopall-Donat <naufalarif09@gmail.com>
2023-11-22 15:46:57 +0700

    main page

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-import "./dist/css/main.css";
+// import "./dist/css/main.css";
 
 import { BrowserRouter } from "react-router-dom";
 
--- src/pages/User/HomePage.jsx
+++ src/pages/User/HomePage.jsx
@@ -1,5 +1,29 @@
+import '../../dist/css/main.css'
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="homepage">
+
+      <div className="home-tittle">
+        <h1>Ingin Mencari kost diarea Jabodetabek?</h1>
+        <h2>Booking Di <span>Kostku</span> aja</h2>
+      </div>
+
+      <div className="search-bar">
+        <input type="text" />
+        <button>Search</button>
+      </div>
+
+      <div className="image-swiper">
+        <div className="shape"></div>
+        <div className="shape"></div>
+        <div className="shape"></div>
+      </div>
+
+      <div className="swipper"></div>
+
+    </div>
+  );
 };
 
 export default HomePage;
