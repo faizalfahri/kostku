374a0f68d14e4b5eff8f527ded385fea864b09f5
faizalfahri <faizfah10@gmail.com>
2023-11-26 06:00:58 +0700

    gambar

--- src/pages/User/HomePage.jsx
+++ src/pages/User/HomePage.jsx
@@ -2,20 +2,26 @@ import NavbarComponent from "../../components/NavbarComponent";
 import FooterComponent from "../../components/FooterComponent";
 
 import { Container } from "react-bootstrap";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+import imgperi from "../../assets/img/KostIbuPeri.jpg";
+import imgbarbie from "../../assets/img/KostIbuBarbie.png";
+import imgsihir from "../../assets/img/KostNenekSihir.jpeg";
 
 const HomePage = () => {
   return (
     <div>
-      <NavbarComponent /> 
+      <NavbarComponent />
 
       {/* Header */}
       <div className="headerpage">
         <Container>
           <div className="header-tittle">
             <h1>Ingin Mencari kost di area Jabodetabek?</h1>
-            <h2>Booking Di <span>Kostku</span> aja.</h2>
+            <h2>
+              Booking Di <span>Kostku</span> aja.
+            </h2>
           </div>
 
           <div className="search-bar">
@@ -43,62 +49,96 @@ const HomePage = () => {
           </div>
 
           <div className="section-card">
-
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={imgperi} />
               <Card.Body>
                 <Card.Title>Kost Ibu Peri</Card.Title>
-                <Button variant="secondary" size="sm" disabled className="mt-1 mb-2">Putri</Button>
+                <Button
+                  variant="secondary"
+                  size="sm"
+                  disabled
+                  className="mt-1 mb-2"
+                >
+                  Putri
+                </Button>
                 <Card.Text className="card-spec">
                   Spesifikasi: <br></br>
                   Kamar Mandi Dalam - Kasur - Wifi - AC
                 </Card.Text>
-                <Card.Text className="card-loc">
+                <Card.Text className="card-loc border-bottom border-dark fw-bold">
                   Jakarta Utara
                 </Card.Text>
-                <Button className="button">Cek</Button>
+                <Card.Text className="card-spec">
+                  Harga Sewa: <br></br>
+                  <span className="fw-bold">Rp.1.200.000/Bulan</span>
+                  <Button className="button ms-4">Cek</Button>
+                </Card.Text>
               </Card.Body>
             </Card>
 
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={imgbarbie} />
               <Card.Body>
-                <Card.Title>Kost Ibu Peri</Card.Title>
-                <Button variant="secondary" size="sm" disabled className="mt-1 mb-2">Putri</Button>
+                <Card.Title>Kost Ibu Barbie</Card.Title>
+                <Button
+                  variant="secondary"
+                  size="sm"
+                  disabled
+                  className="mt-1 mb-2"
+                >
+                  Putri
+                </Button>
                 <Card.Text className="card-spec">
                   Spesifikasi: <br></br>
                   Kamar Mandi Dalam - Kasur - Wifi - AC
                 </Card.Text>
-                <Card.Text className="card-loc">
+                <Card.Text className="card-loc border-bottom border-dark fw-bold">
                   Jakarta Utara
                 </Card.Text>
-                <Button className="button">Cek</Button>
+                <Card.Text className="card-spec">
+                  Harga Sewa: <br></br>
+                  <span className="fw-bold">Rp.1.200.000/Bulan</span>
+                  <Button className="button ms-4">Cek</Button>
+                </Card.Text>
               </Card.Body>
             </Card>
 
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={imgsihir} />
               <Card.Body>
-                <Card.Title>Kost Ibu Peri</Card.Title>
-                <Button variant="secondary" size="sm" disabled className="mt-1 mb-2">Putri</Button>
+                <Card.Title>Kost Nenek Sihir</Card.Title>
+                <Button
+                  variant="secondary"
+                  size="sm"
+                  disabled
+                  className="mt-1 mb-2"
+                >
+                  Putri
+                </Button>
                 <Card.Text className="card-spec">
                   Spesifikasi: <br></br>
                   Kamar Mandi Dalam - Kasur - Wifi - AC
                 </Card.Text>
-                <Card.Text className="card-loc">
+                <Card.Text className="card-loc border-bottom border-dark fw-bold">
                   Jakarta Utara
                 </Card.Text>
-                <Button className="button">Cek</Button>
+                <Card.Text className="card-spec">
+                  Harga Sewa: <br></br>
+                  <span className="fw-bold">Rp.1.200.000/Bulan</span>
+                  <Button className="button ms-4">Cek</Button>
+                </Card.Text>
               </Card.Body>
             </Card>
-            
+          </div>
+
+          <div className="section-tittle">
+            <Button className="button">Lainnya</Button>
           </div>
         </Container>
       </div>
       {/* End Section Kost */}
 
-
-      <FooterComponent />
+      {/* <FooterComponent /> */}
     </div>
   );
 };
