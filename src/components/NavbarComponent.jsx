acbf01601988158020dfb16a9414c05a85ae7023
faizalfahri <faizfah10@gmail.com>
2023-12-07 09:05:38 +0700

    test cpu 2

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,30 +1,39 @@
-import { Navbar, Container, Nav, Button } from "react-bootstrap"
-import logo from './../assets/img/kostku-hitam.png'
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import logo from "./../assets/img/kostku-hitam.png";
 
 const NavbarComponent = () => {
-  return <div>
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">
-          <img
-            src={logo}
-            width="30"
-            height="30"
-            className="d-inline-block align-top logo"
-            alt="kostku logo"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#kost" className="nav-link">Cari Kost</Nav.Link>
-            <Nav.Link href="#tentang" className="nav-link">Tentang Kami</Nav.Link>
-            <Button className="ms-3 button">Masuk</Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  </div>;
+  return (
+    <div>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top logo"
+              alt="kostku logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#kost" className="nav-link">
+                Cari Kost
+              </Nav.Link>
+              <Nav.Link href="#tentang" className="nav-link">
+                Tentang Kami
+              </Nav.Link>
+              <Link to={"/login"} className="ms-3 btn button text-light">
+                Masuk
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default NavbarComponent;
